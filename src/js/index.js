d96ea95c769f1e4d66a344d57d8b4fea8cfd79ec
DANIEL REVO <DANIELREVO08@GMAIL.COM>
2016-03-09 21:35:43 -0500

    implemented approach demonstrated in class. Now trying surge deployment again.

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,19 @@
 import $ from 'jquery';
 
 
+
+/////////Retrying using the strategy from class
+
+$('h2').click(function(e){
+	$('.sectionContainer div').addClass('closed');
+
+	var $currentH2 = $(event.currentTarget);
+
+	$currentH2.next().removeClass('closed');
+})
+
+
+
 // $('.section1').on('click', function() {
 //   $('.section2').toggleClass('altPosition2');
 //   $('.section3').toggleClass('altPosition3');
@@ -22,55 +35,55 @@ import $ from 'jquery';
 
 /////After trying to figure out a jquery approach that ensured sections wouldn't ever overlap regardless of height and click order, I tried this very long javascript approach
 
-var section1 = document.querySelector(".section1");
-var section2 = document.querySelector(".section2");
-var section3 = document.querySelector(".section3");
+// var section1 = document.querySelector(".section1");
+// var section2 = document.querySelector(".section2");
+// var section3 = document.querySelector(".section3");
 
-var secTwoDefaultPosition = true;
-var secThreeDefaultPosition = true;
+// var secTwoDefaultPosition = true;
+// var secThreeDefaultPosition = true;
 
-section1.addEventListener('click', function(e){
+// section1.addEventListener('click', function(e){
 	
-	if(secTwoDefaultPosition || secThreeDefaultPosition){
-		section2.style.top = section1.offsetHeight + "px";
-		section3.style.top = (section1.offsetHeight + 80) + "px";
-		secThreeDefaultPosition = false;
-		secTwoDefaultPosition = false;
+// 	if(secTwoDefaultPosition || secThreeDefaultPosition){
+// 		section2.style.top = section1.offsetHeight + "px";
+// 		section3.style.top = (section1.offsetHeight + 80) + "px";
+// 		secThreeDefaultPosition = false;
+// 		secTwoDefaultPosition = false;
 
-	}else{
-		section2.style.top = "80px";
-		section3.style.top = "160px";
-		secThreeDefaultPosition = true;
-		secTwoDefaultPosition = true;
-	}
-})
+// 	}else{
+// 		section2.style.top = "80px";
+// 		section3.style.top = "160px";
+// 		secThreeDefaultPosition = true;
+// 		secTwoDefaultPosition = true;
+// 	}
+// })
 
-section2.addEventListener('click', function(e){
-	if(secTwoDefaultPosition && secThreeDefaultPosition){
-		section3.style.top = (section2.offsetHeight + 80) + "px";
-		secThreeDefaultPosition = false;
-		secTwoDefaultPosition = false;
-	}else if(!secTwoDefaultPosition){
-		section2.style.top = "80px";
-		secTwoDefaultPosition = true;
-		section3.style.top = (section2.offsetHeight + 80) + "px";
-		secThreeDefaultPosition = false;
-	}
-	else{
-		section3.style.top = "160px";
-		secThreeDefaultPosition = true;
-		secTwoDefaultPosition = true;
-	}
-})
+// section2.addEventListener('click', function(e){
+// 	if(secTwoDefaultPosition && secThreeDefaultPosition){
+// 		section3.style.top = (section2.offsetHeight + 80) + "px";
+// 		secThreeDefaultPosition = false;
+// 		secTwoDefaultPosition = false;
+// 	}else if(!secTwoDefaultPosition){
+// 		section2.style.top = "80px";
+// 		secTwoDefaultPosition = true;
+// 		section3.style.top = (section2.offsetHeight + 80) + "px";
+// 		secThreeDefaultPosition = false;
+// 	}
+// 	else{
+// 		section3.style.top = "160px";
+// 		secThreeDefaultPosition = true;
+// 		secTwoDefaultPosition = true;
+// 	}
+// })
 
-section3.addEventListener('click', function(e){
-	if(!secThreeDefaultPosition){
-		section3.style.top = "160px";
-		section2.style.top = "80px";
-		secTwoDefaultPosition = true;
-		secThreeDefaultPosition = true;
-	}else{
-		section3.style.top = (section2.offsetHeight + 80) + "px";
-		secThreeDefaultPosition = false;
-	}
-})
+// section3.addEventListener('click', function(e){
+// 	if(!secThreeDefaultPosition){
+// 		section3.style.top = "160px";
+// 		section2.style.top = "80px";
+// 		secTwoDefaultPosition = true;
+// 		secThreeDefaultPosition = true;
+// 	}else{
+// 		section3.style.top = (section2.offsetHeight + 80) + "px";
+// 		secThreeDefaultPosition = false;
+// 	}
+// })
